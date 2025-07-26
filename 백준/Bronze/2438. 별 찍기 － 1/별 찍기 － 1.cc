5aba62acff862e9c12482beaa46d5da00eaac86f
PLAN <114268720+plan6062@users.noreply.github.com>
2025-07-26 16:26:43 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i < N + 1; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << '*';
+        }
+        if (i == N)
+        {
+            break;
+        }
+        else
+        {
+            cout << endl;
+        }
+    }
+    return 0;
+}
