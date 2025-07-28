356eaba909f1516b415e9eccef04f1b8ddf04e89
PLAN <114268720+plan6062@users.noreply.github.com>
2025-07-28 16:34:03 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B;
+
+    while (true)
+    {
+        cin >> A >> B;
+        if (A == 0 && B == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << A + B << "\n";
+        }
+    }
+    return 0;
+}
