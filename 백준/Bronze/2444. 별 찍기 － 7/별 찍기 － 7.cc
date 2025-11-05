8ed075a059ba7adb60cceff5d1f51bb2ac31f00f
Taeyun LEE <114268720+plan6062@users.noreply.github.com>
2025-11-05 14:00:21 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i = 1; i < N; i++){
+        for(int j = i; j < N; j++){
+            cout << " ";
+        }
+        for(int k = 0; k<2*i -1; k++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int i = N; i > 0; i--){
+        for(int j = i; j < N; j++){
+            cout << " ";
+        }
+        for(int k = 0; k < 2*i-1; k++){
+            cout << "*";
+        }
+        if(i != 1){
+            cout << "\n";
+        }
+    }
+    return 0;
+}
