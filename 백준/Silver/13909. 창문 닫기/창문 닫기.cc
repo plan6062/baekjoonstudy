08070ffd2b39e8e19f5af5b19a84fa31459ad3f6
Taeyun LEE <114268720+plan6062@users.noreply.github.com>
2025-12-15 17:34:48 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long N;
+    cin >> N;
+    cout << (long long)sqrt(N) << "\n";
+    return 0;
+}
